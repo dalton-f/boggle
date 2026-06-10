6a06bdcaacec39493f2f3e6e25869fc8c015356d
Dalton <daltonfiggdavies@gmail.com>
2026-06-10 11:02:34 +0100

    Basic form submission setup

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,93 @@
+/* eslint-disable no-magic-numbers */
+
+// Post 2012 standard dice used for 4x4 Boggle
+const BOGGLE_DICE = [
+  ["a", "a", "e", "e", "g", "n"],
+  ["a", "b", "b", "j", "o", "o"],
+  ["a", "c", "h", "o", "p", "s"],
+  ["a", "f", "f", "k", "p", "s"],
+  ["a", "o", "o", "t", "t", "w"],
+  ["c", "i", "m", "o", "t", "u"],
+  ["d", "e", "i", "l", "r", "x"],
+  ["d", "e", "l", "r", "v", "y"],
+  ["d", "i", "s", "t", "t", "y"],
+  ["e", "e", "g", "h", "n", "w"],
+  ["e", "e", "i", "n", "s", "u"],
+  ["e", "h", "r", "t", "v", "w"],
+  ["e", "i", "o", "s", "s", "t"],
+  ["e", "l", "r", "t", "t", "y"],
+  ["h", "i", "m", "n", "qu", "u"],
+  ["h", "l", "n", "n", "r", "z"],
+];
+
+// Post 2012 standard dice used for 5x5 Big Boggle
+const BOGGLE_BIG_DICE = [
+  ["a", "a", "a", "f", "r", "s"],
+  ["a", "a", "e", "e", "e", "e"],
+  ["a", "a", "f", "i", "r", "s"],
+  ["a", "d", "e", "n", "n", "n"],
+  ["a", "e", "e", "e", "e", "m"],
+  ["a", "e", "e", "g", "m", "u"],
+  ["a", "e", "g", "m", "n", "n"],
+  ["a", "f", "i", "r", "s", "y"],
+  ["b", "b", "j", "k", "x", "z"],
+  ["c", "c", "e", "n", "s", "t"],
+  ["e", "i", "i", "l", "s", "t"],
+  ["c", "e", "i", "p", "s", "t"],
+  ["d", "d", "h", "n", "o", "t"],
+  ["d", "h", "h", "l", "o", "r"],
+  ["d", "h", "h", "n", "o", "w"],
+  ["d", "h", "l", "n", "o", "r"],
+  ["e", "i", "i", "i", "t", "t"],
+  ["e", "i", "l", "p", "s", "t"],
+  ["e", "m", "o", "t", "t", "t"],
+  ["e", "n", "s", "s", "s", "u"],
+  ["an", "er", "he", "in", "qu", "th"],
+  ["g", "o", "r", "r", "v", "w"],
+  ["i", "p", "r", "s", "y", "y"],
+  ["n", "o", "o", "t", "u", "w"],
+  ["o", "o", "o", "t", "t", "u"],
+];
+
+const BOGGLE_SUPER_BIG_DICE = [
+  ["a", "a", "a", "f", "r", "s"],
+  ["a", "a", "e", "e", "e", "e"],
+  ["a", "a", "e", "e", "o", "o"],
+  ["a", "a", "f", "i", "r", "s"],
+  ["a", "b", "d", "e", "i", "o"],
+  ["a", "d", "e", "n", "n", "n"],
+  ["a", "e", "e", "e", "e", "m"],
+  ["a", "e", "e", "g", "m", "u"],
+  ["a", "e", "g", "m", "n", "n"],
+  ["a", "e", "i", "l", "m", "n"],
+  ["a", "e", "i", "n", "o", "u"],
+  ["a", "f", "i", "r", "s", "y"],
+  ["an", "er", "he", "in", "qu", "th"],
+  ["b", "b", "j", "k", "x", "z"],
+  ["c", "c", "e", "n", "s", "t"],
+  ["c", "d", "d", "l", "n", "n"],
+  ["c", "e", "i", "i", "t", "t"],
+  ["c", "e", "i", "p", "s", "t"],
+  ["c", "f", "g", "n", "u", "y"],
+  ["d", "d", "h", "n", "o", "t"],
+  ["d", "h", "h", "l", "o", "r"],
+  ["d", "h", "h", "n", "o", "w"],
+  ["d", "h", "l", "n", "o", "r"],
+  ["e", "h", "i", "l", "r", "s"],
+  ["e", "i", "i", "l", "s", "t"],
+  ["e", "i", "l", "p", "s", "t"],
+  ["e", "i", "o", "-", "-", "-"],
+  ["e", "m", "t", "t", "t", "o"],
+  ["e", "n", "s", "s", "s", "u"],
+  ["g", "o", "r", "r", "v", "w"],
+  ["h", "i", "r", "s", "t", "v"],
+  ["h", "o", "p", "r", "s", "t"],
+  ["i", "p", "r", "s", "y", "y"],
+  ["j", "k", "qu", "w", "x", "z"],
+  ["n", "o", "o", "t", "u", "w"],
+  ["o", "o", "o", "t", "t", "u"],
+];
+
 const gridSizeDecreaseButton = document.getElementById(
   "gridSizeDecreaseButton",
 );
@@ -17,6 +107,8 @@ const timeLimitIncreaseButton = document.getElementById(
 );
 
 const timeLimitSelector = document.getElementById("timeLimitSelector");
+
+const settingsForm = document.getElementById("settingsForm");
 
 const setupSelectorControls = (decreaseButton, increaseButton, selector) => {
   decreaseButton.addEventListener("click", () => {
@@ -40,3 +132,13 @@ setupSelectorControls(
   timeLimitIncreaseButton,
   timeLimitSelector,
 );
+
+settingsForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  startNewBoggleGame(gridSizeSelector.value, timeLimitSelector.value);
+});
+
+const startNewBoggleGame = (gridSize, timeLimit) => {
+  console.log(`Grid Size: ${gridSize} | Time Limit: ${timeLimit}`);
+};
--- static/js/app.js
+++ static/js/app.js
@@ -20,12 +20,21 @@ __webpack_require__.r(__webpack_exports__);
   \***********************/
 /***/ (() => {
 
+/* eslint-disable no-magic-numbers */
+
+// Post 2012 standard dice used for 4x4 Boggle
+var BOGGLE_DICE = [["a", "a", "e", "e", "g", "n"], ["a", "b", "b", "j", "o", "o"], ["a", "c", "h", "o", "p", "s"], ["a", "f", "f", "k", "p", "s"], ["a", "o", "o", "t", "t", "w"], ["c", "i", "m", "o", "t", "u"], ["d", "e", "i", "l", "r", "x"], ["d", "e", "l", "r", "v", "y"], ["d", "i", "s", "t", "t", "y"], ["e", "e", "g", "h", "n", "w"], ["e", "e", "i", "n", "s", "u"], ["e", "h", "r", "t", "v", "w"], ["e", "i", "o", "s", "s", "t"], ["e", "l", "r", "t", "t", "y"], ["h", "i", "m", "n", "qu", "u"], ["h", "l", "n", "n", "r", "z"]];
+
+// Post 2012 standard dice used for 5x5 Big Boggle
+var BOGGLE_BIG_DICE = [["a", "a", "a", "f", "r", "s"], ["a", "a", "e", "e", "e", "e"], ["a", "a", "f", "i", "r", "s"], ["a", "d", "e", "n", "n", "n"], ["a", "e", "e", "e", "e", "m"], ["a", "e", "e", "g", "m", "u"], ["a", "e", "g", "m", "n", "n"], ["a", "f", "i", "r", "s", "y"], ["b", "b", "j", "k", "x", "z"], ["c", "c", "e", "n", "s", "t"], ["e", "i", "i", "l", "s", "t"], ["c", "e", "i", "p", "s", "t"], ["d", "d", "h", "n", "o", "t"], ["d", "h", "h", "l", "o", "r"], ["d", "h", "h", "n", "o", "w"], ["d", "h", "l", "n", "o", "r"], ["e", "i", "i", "i", "t", "t"], ["e", "i", "l", "p", "s", "t"], ["e", "m", "o", "t", "t", "t"], ["e", "n", "s", "s", "s", "u"], ["an", "er", "he", "in", "qu", "th"], ["g", "o", "r", "r", "v", "w"], ["i", "p", "r", "s", "y", "y"], ["n", "o", "o", "t", "u", "w"], ["o", "o", "o", "t", "t", "u"]];
+var BOGGLE_SUPER_BIG_DICE = [["a", "a", "a", "f", "r", "s"], ["a", "a", "e", "e", "e", "e"], ["a", "a", "e", "e", "o", "o"], ["a", "a", "f", "i", "r", "s"], ["a", "b", "d", "e", "i", "o"], ["a", "d", "e", "n", "n", "n"], ["a", "e", "e", "e", "e", "m"], ["a", "e", "e", "g", "m", "u"], ["a", "e", "g", "m", "n", "n"], ["a", "e", "i", "l", "m", "n"], ["a", "e", "i", "n", "o", "u"], ["a", "f", "i", "r", "s", "y"], ["an", "er", "he", "in", "qu", "th"], ["b", "b", "j", "k", "x", "z"], ["c", "c", "e", "n", "s", "t"], ["c", "d", "d", "l", "n", "n"], ["c", "e", "i", "i", "t", "t"], ["c", "e", "i", "p", "s", "t"], ["c", "f", "g", "n", "u", "y"], ["d", "d", "h", "n", "o", "t"], ["d", "h", "h", "l", "o", "r"], ["d", "h", "h", "n", "o", "w"], ["d", "h", "l", "n", "o", "r"], ["e", "h", "i", "l", "r", "s"], ["e", "i", "i", "l", "s", "t"], ["e", "i", "l", "p", "s", "t"], ["e", "i", "o", "-", "-", "-"], ["e", "m", "t", "t", "t", "o"], ["e", "n", "s", "s", "s", "u"], ["g", "o", "r", "r", "v", "w"], ["h", "i", "r", "s", "t", "v"], ["h", "o", "p", "r", "s", "t"], ["i", "p", "r", "s", "y", "y"], ["j", "k", "qu", "w", "x", "z"], ["n", "o", "o", "t", "u", "w"], ["o", "o", "o", "t", "t", "u"]];
 var gridSizeDecreaseButton = document.getElementById("gridSizeDecreaseButton");
 var gridSizeIncreaseButton = document.getElementById("gridSizeIncreaseButton");
 var gridSizeSelector = document.getElementById("gridSizeSelector");
 var timeLimitDecreaseButton = document.getElementById("timeLimitDecreaseButton");
 var timeLimitIncreaseButton = document.getElementById("timeLimitIncreaseButton");
 var timeLimitSelector = document.getElementById("timeLimitSelector");
+var settingsForm = document.getElementById("settingsForm");
 var setupSelectorControls = function setupSelectorControls(decreaseButton, increaseButton, selector) {
   decreaseButton.addEventListener("click", function () {
     if (selector.selectedIndex > 0) selector.selectedIndex--;
@@ -36,6 +45,13 @@ var setupSelectorControls = function setupSelectorControls(decreaseButton, incre
 };
 setupSelectorControls(gridSizeDecreaseButton, gridSizeIncreaseButton, gridSizeSelector);
 setupSelectorControls(timeLimitDecreaseButton, timeLimitIncreaseButton, timeLimitSelector);
+settingsForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  startNewBoggleGame(gridSizeSelector.value, timeLimitSelector.value);
+});
+var startNewBoggleGame = function startNewBoggleGame(gridSize, timeLimit) {
+  console.log("Grid Size: ".concat(gridSize, " | Time Limit: ").concat(timeLimit));
+};
 
 /***/ })
 
